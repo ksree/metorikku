265911547aec933e647230cee1da1cdd1c08e8cc
lyogev <lyogev@yotpo.com>
2020-06-20 20:51:23 +0300

    feat(spark): upgrade to spark 3.0.0

--- scripts/docker_load_dev.sh
+++ scripts/docker_load_dev.sh
@@ -7,10 +7,8 @@ docker pull metorikku/dev:metorikku_$tag
 docker pull metorikku/dev:spark_$tag
 docker pull metorikku/dev:k8s-spark-operator_$tag
 docker pull metorikku/dev:hive_$tag
-docker pull metorikku/dev:hive1_$tag
 
 docker tag metorikku/dev:metorikku_$tag metorikku/metorikku
 docker tag metorikku/dev:spark_$tag metorikku/spark
 docker tag metorikku/dev:k8s-spark-operator_$tag metorikku/k8s-spark-operator
 docker tag metorikku/dev:hive_$tag metorikku/hive
-docker tag metorikku/dev:hive1_$tag metorikku/hive:1
